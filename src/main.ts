c0962fdc53ae9cfed31f537fc12f27ac25634216
Mubashir Imtiaz <mubashir@sudofy.com>
2022-08-24 18:26:37 +0500

    added and integrated swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { TransformResInterceptor } from './common/interceptors';
 
@@ -15,6 +16,16 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('FUMA API')
+    .setDescription('FUMA API description')
+    .setVersion('1.0')
+    .addTag('core')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/v1/api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
