25a12c96aa68b709a3d1f431b2ceb6e4a0518a55
Mubashir Imtiaz <56713252+mubashirimtiaz@users.noreply.github.com>
2022-09-26 11:32:25 +0500

    fix(global): return tokens on forget password and verify email

--- src/user/type/user.type.ts
+++ src/user/type/user.type.ts
@@ -1,4 +1,12 @@
+import { User } from '../interface/user.interface';
+
 export enum PASSWORD_CHANGE_TYPE {
   UPDATE = 'UPDATE',
   FORGET = 'FORGET',
 }
+
+export type AuthToken = {
+  refreshToken?: string;
+  accessToken: string;
+  user: User;
+};
